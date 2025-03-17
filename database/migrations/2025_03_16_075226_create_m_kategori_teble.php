55a8ccbfa46a549a8e0d403967022408183ca152
vera efita hudi putri <veraefita05@gmail.com>
2025-03-17 21:19:27 +0700

    prak 2.2

--- database/migrations/2025_03_16_075226_create_m_kategori_teble.php
+++ database/migrations/2025_03_16_075226_create_m_kategori_teble.php
@@ -4,26 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('m_kategori_teble', function (Blueprint $table) {
-            $table->id();
-            $table->string('level_kode', 10)->unique();
-            $table->string('level_nama', 100);
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('m_kategori', function (Blueprint $table) {
+            $table->id('kategori_id'); // Primary key
+            $table->string('kategori_kode', 10)->unique();
+            $table->string('kategori_nama', 100);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('m_kategori_teble');
+    public function down(): void {
+        Schema::dropIfExists('m_kategori');
     }
 };
