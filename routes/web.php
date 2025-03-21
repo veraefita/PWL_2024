e0150695e3946c69666b54b56b8eb0d9d2008836
vera efita hudi putri <veraefita05@gmail.com>
2025-03-21 15:20:33 +0700

    prak5

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use Database\Seeders\KategoriSeeder;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,7 +15,12 @@ Route::get('/', function () {
 
 // Route::get('/update-level', [LevelController::class, 'index']);
 // Route::get('/delete-level', [LevelController::class, 'index']);
-Route::get('/data-level', [LevelController::class, 'index']);
+// Route::get('/data-level', [LevelController::class, 'index']);
+Route::get('/data-kategori', [KategoriController::class, 'index']);
+
+
+
+
 
 
 
